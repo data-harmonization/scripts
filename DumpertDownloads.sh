f98c570f9fd1cef804131ca7ab945060ed656d65
Paul Stapersma <paul.stapersma@tno.nl>
2018-02-04 20:40:11 +0100

    added tree-statement

--- DumpertDownloads.sh
+++ DumpertDownloads.sh
@@ -2,7 +2,7 @@
 if [ $(ls -1 ~/Downloads/ | grep dump |  wc -l) -gt 0  ]
 then
 	echo "Moving the following files:"
-	echo ~/Downloads/dumpert*
+	tree -iQf ~/Downloads/ | grep dumpert
 	mv ~/Downloads/dumpert*.mp4 /Users/p.stapersma/odrive/Amazon\ Cloud\ Drive/Personal/Dumpert
 	ls /Users/p.stapersma/odrive/Amazon\ Cloud\ Drive/Personal/Dumpert/*.mp4 | while read x; do python ~/.odrive/bin/6352/odrive.py unsync "$x"; done
 fi
