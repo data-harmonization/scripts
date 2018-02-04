99853bbf4ed79e817ff10b4b49bbeb15947f5439
Paul Stapersma <paul.stapersma@tno.nl>
2018-02-04 21:44:11 +0100

    changed call to odrive

--- DumpertDownloads.sh
+++ DumpertDownloads.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+alias odrive='python $(ls -d "$HOME/.odrive/bin/"*/ | tail -1)/odrive.py'
 if [ $(ls -1 ~/Downloads/ | grep dump |  wc -l) -gt 0  ]
 then
 	echo "Moving the following files:"
 	tree -iQf ~/Downloads/ | grep dumpert
 	mv ~/Downloads/dumpert*.mp4 /Users/p.stapersma/odrive/Amazon\ Cloud\ Drive/Personal/Dumpert
-	ls /Users/p.stapersma/odrive/Amazon\ Cloud\ Drive/Personal/Dumpert/*.mp4 | while read x; do python ~/.odrive/bin/6352/odrive.py unsync "$x"; done
+	ls /Users/p.stapersma/odrive/Amazon\ Cloud\ Drive/Personal/Dumpert/*.mp4 | while read x; do odrive unsync "$x"; done
 fi
